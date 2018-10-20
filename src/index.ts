85958e1920b77c7c4d4aef52fce7200b2beacd4f
dennisat <info@anel.co>
2018-10-20 12:18:07 +0200

    feature: icons

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,38 @@ if (isNode){
   const cw = console.warn;
   const ce = console.error;
 
+  enum ELogType {
+    LOG ="LOG",
+    INFO ="INFO",
+    DEBUG ="DEBUG",
+    WARN ="WARN",
+    ERROR ="ERROR",
+    TIME ="TIME",
+  }
+
   const isTime = (args: any[]): boolean => args[0] === "%s: %sms";
   const isFormatted = (args: any[]): boolean => args.length > 1 && typeof args[0] === "string" && args[0].indexOf("%") > -1;
+  const getLabel = (logType:ELogType)=>{
+    switch (logType) {
+      case ELogType.LOG: return '⚫ LOG  ';
+      case ELogType.INFO: return '🔵 INFO ';
+      case ELogType.DEBUG: return '🐞 DEBUG';
+      case ELogType.WARN: return '🔶 WARN ';
+      case ELogType.ERROR: return '🔴 ERROR';
+      case ELogType.TIME: return '⏱ TIME ';
+    }
+  };
 
-  const buildArgs = (prefix: string, args: any[]): any[] => {
-    debugger;
+  const buildArgs = (logType:ELogType, args: any[]): any[] => {
     const now: Date = new Date;
     const ms: number = now.getMilliseconds();
     const timeStamp = `${(new Date).toLocaleString()}.${ms}`;
     const _isTime = isTime(args);
     const _isFormatted = isFormatted(args);
 
-    if (_isTime) prefix = "t TIME ";
+    if (_isTime) logType=ELogType.TIME;
 
-    prefix = `${prefix} ${timeStamp}`;
+    let prefix: string = `${getLabel(logType)} ${timeStamp}`;
 
     if (_isFormatted) {
       return [].concat(`${prefix} ${args[0]}`, args.slice(1))
@@ -30,9 +48,9 @@ if (isNode){
     }
   };
 
-  console.log = (...args): void => cl(...buildArgs("  LOG  ", args));
-  console.info = (...args): void => ci(...buildArgs("i INFO ", args));
-  console.debug = (...args): void => cd(...buildArgs("> DEBUG", args));
-  console.warn = (...args): void => cw(...buildArgs("i WARN ", args));
-  console.error = (...args): void => ce(...buildArgs("i ERROR", args));
+  console.log = (...args): void => cl(...buildArgs(ELogType.LOG, args));
+  console.info = (...args): void => ci(...buildArgs(ELogType.INFO, args));
+  console.debug = (...args): void => cd(...buildArgs(ELogType.DEBUG, args));
+  console.warn = (...args): void => cw(...buildArgs(ELogType.WARN, args));
+  console.error = (...args): void => ce(...buildArgs(ELogType.ERROR, args));
 }
