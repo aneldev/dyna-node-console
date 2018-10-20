8844176a5a959f864231c2dfdf51763b2f33c3fa
dennisat <info@anel.co>
2018-10-20 11:49:22 +0200

    fix: support formatted consoles

--- dev/index.ts
+++ dev/index.ts
@@ -1,9 +1,14 @@
 import "../utils/unhandledPromiseRejections";
 import "../src"
 
+console.time('Time elapsed');
 console.log('Example of log');
 console.info('Example of info');
 console.warn('Example of warn');
 console.error('Example of error');
 console.debug('Example of debug');
-console.log('Example of object arg', {objectSample: {name: "John"}});
+console.log('Text and object', {objectSample: {name: "John"}});
+console.log({objectSample: {name: "John"}}, "object and text");
+console.log({objectSample: {name: "John"}}, {objectSample: {name: "John"}});
+console.log();
+console.timeEnd('Time elapsed');
--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,32 @@ if (isNode){
   const cw = console.warn;
   const ce = console.error;
 
-  console.log = (...args): void => cl(`  LOG   ${(new Date).toLocaleString()}`, ...args);
-  console.info = (...args): void => ci(`i INFO  ${(new Date).toLocaleString()}`, ...args);
-  console.debug = (...args): void => cd(`> DEBUG ${(new Date).toLocaleString()}`, ...args);
-  console.warn = (...args): void => cw(`! WARN  ${(new Date).toLocaleString()}`, ...args);
-  console.error = (...args): void => ce(`X ERROR ${(new Date).toLocaleString()}`, ...args);
+  const isTime = (args: any[]): boolean => args[0] === "%s: %sms";
+  const isFormatted = (args: any[]): boolean => args.length > 1 && typeof args[0] === "string" && args[0].indexOf("%") > -1;
+
+  const buildArgs = (prefix: string, args: any[]): any[] => {
+    debugger;
+    const now: Date = new Date;
+    const ms: number = now.getMilliseconds();
+    const timeStamp = `${(new Date).toLocaleString()}.${ms}`;
+    const _isTime = isTime(args);
+    const _isFormatted = isFormatted(args);
+
+    if (_isTime) prefix = "t TIME ";
+
+    prefix = `${prefix} ${timeStamp}`;
+
+    if (_isFormatted) {
+      return [].concat(`${prefix} ${args[0]}`, args.slice(1))
+    }
+    else {
+      return [].concat(prefix, args);
+    }
+  };
+
+  console.log = (...args): void => cl(...buildArgs("  LOG  ", args));
+  console.info = (...args): void => ci(...buildArgs("i INFO ", args));
+  console.debug = (...args): void => cd(...buildArgs("> DEBUG", args));
+  console.warn = (...args): void => cw(...buildArgs("i WARN ", args));
+  console.error = (...args): void => ce(...buildArgs("i ERROR", args));
 }
